9f0d49c1c8806a72f37073284f173537ec14eb3a
6btkp <theaaryanto2004@gmail.com>
2022-09-11 15:47:20 +0700

    second commit

--- katalog/views.py
+++ katalog/views.py
@@ -6,6 +6,8 @@ from katalog.models import CatalogItem
 def show_katalog(request):
     data_katalog = CatalogItem.objects.all()
     context = {
-    'list_katalog': data_katalog
+    'list_katalog': data_katalog,
+    'nama': 'Clarissa Thea Aryanto',
+    'id': '2106634673'
     }
     return render(request, "katalog.html", context)
