6b27474bd10ab9f9cf0208d224aa05e8b3d5ce2a
6btkp <theaaryanto2004@gmail.com>
2022-09-11 15:32:12 +0700

    first commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+    'list_katalog': data_katalog
+    }
+    return render(request, "katalog.html", context)
