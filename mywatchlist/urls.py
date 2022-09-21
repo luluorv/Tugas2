f801da797ee99c5c66cc4a619ab9bc5970a0339e
6btkp <theaaryanto2004@gmail.com>
2022-09-21 22:46:54 +0700

    tugas 3

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,10 +1,12 @@
 # TODO: Implement Routings Here
 
 from django.urls import path
-from mywatchlist.views import show_watchlist
+from mywatchlist.views import show_html, show_xml, show_json
 
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_watchlist, name='show_mywatchlist'),
+    path('html/', show_html, name='show_html'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from mywatchlist.models import WatchlistItem
+from django.http import HttpResponse
+from django.core import serializers
 
 # TODO: Create your views here.
 
-def show_watchlist(request):
+def show_html(request):
     watchlistitem = WatchlistItem.objects.all()
     watched = WatchlistItem.objects.filter(watched = True).count()
     not_watched = WatchlistItem.objects.filter(watched = False).count()
-    
+    count = watched >= not_watched
+
+
     context = {
     'list_watchlist': watchlistitem,
     'nama': 'Clarissa Thea Aryanto',
-    'id': '2106634673'
+    'id': '2106634673',
+    'watched_a_lot': count
     }
     return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = WatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = WatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
